4a1d5da51db521e13d55e27ac068bf6bef091e82
Otis Lin <hrlin@digitalriver.com>
2016-09-14 20:42:25 +0800

    init commit

---
+++ src/main/java/com/otis/webapp/HomeController.java
@@ -0,0 +1,53 @@
+package com.otis.webapp;
+
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/post", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<Object> getData(HttpServletRequest request) {
+		logger.info(request.getHeader("Accept"));
+		
+		return new ResponseEntity<Object>(Arrays.asList("a", "b", "c"), HttpStatus.OK);
+	}
+	
+}
